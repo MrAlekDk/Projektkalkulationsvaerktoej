5d379a4be4321e2d6549352a343f53c65bfaf836
mettemariehws <mettemhws@gmail.com>
2021-05-11 12:46:22 +0200

    SubTaskRep, User, UserRep oprettet.

--- src/main/java/com/example/webapp/repository/SubTaskRep.java
+++ src/main/java/com/example/webapp/repository/SubTaskRep.java
@@ -4,17 +4,22 @@ import com.example.webapp.models.Worker;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Properties;
 
-public class WorkerRep {
+public class SubTaskRep {
+
     private String url;
     private String user;
     private String password;
     private ArrayList<Worker> workers;
 
-    public WorkerRep() {
+    public SubTaskRep() {
         Properties prop = new Properties();
         try {
             prop.load(new FileInputStream("src/main/resources/application.properties"));
@@ -26,13 +31,18 @@ public class WorkerRep {
         }
     }
 
-    public void getPosition(String position, int hourlyRate){
-
+    public void createSubTask(int Subtask_ID, String title, String desc, int worker_ID, Date subtaskStart, int duration, Date subtaskEnd){
         try {
             Connection conn = DriverManager.getConnection(url,user,password);
-            PreparedStatement pstmt = conn.prepareStatement("SELECT Worker (Position,HourlyRate) VALUES (?,?)");
-            ResultSet rs = pstmt.executeQuery();
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO SubTask (SubtaskID, Title, Desc, Worker_ID, SubtaskStart, Duration, SubtaskEnd) VALUES (?,?,?,?,?,?,?)");
 
+            pstmt.setInt(1, Subtask_ID);
+            pstmt.setString(2, title);
+            pstmt.setString(3, desc);
+            pstmt.setInt(4, worker_ID);
+            pstmt.setDate(5, (java.sql.Date) subtaskStart);
+            pstmt.setInt(6, duration);
+            pstmt.setDate(7,(java.sql.Date) subtaskEnd);
             pstmt.executeUpdate();
 
         } catch (SQLException e) {
