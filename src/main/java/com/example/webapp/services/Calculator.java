d86e98912409e9e1a645482ac04ef3c0e4c39927
mettemariehws <mettemhws@gmail.com>
2021-05-26 16:24:35 +0200

    Footer added also on view

--- src/main/java/com/example/webapp/services/Calculator.java
+++ src/main/java/com/example/webapp/services/Calculator.java
@@ -10,52 +10,43 @@ public class Calculator {
     private int hourPrice = 950;
     private int workDayHours = 8;
 
-
     public int getPriceForProject(int projectHours) {
         int projectPrice = hourPrice * projectHours;
         return projectPrice;
     }
+
     public int dailyWorkHours(ArrayList<Task> tasks, Date deadline) {
+        if(tasks.isEmpty()||tasks.get(0).getSubtasks().isEmpty()){
+            return 0;
+        }
+        Date startDate = tasks.get(0).getSubtasks().get(0).getStart();
+        Calendar start = Calendar.getInstance();
+        Calendar dead = Calendar.getInstance();
 
+        start.setTime(startDate);
+        dead.setTime(deadline);
         int days = 0;
-        int workHours=0;
-        try{
-            Date startDate = tasks.get(0).getSubtasks().get(0).getStart();
-            Calendar start = Calendar.getInstance();
-            Calendar dead = Calendar.getInstance();
-
-            start.setTime(startDate);
-            dead.setTime(deadline);
-
-            while (start.before(dead)) {
-                if (Calendar.SATURDAY != start.get(Calendar.DAY_OF_WEEK) && Calendar.SUNDAY != start.get(Calendar.DAY_OF_WEEK)) {
-                    days++;
-                }
-                start.add(Calendar.DATE, 1);
+        while (start.before(dead)) {
+            if (Calendar.SATURDAY != start.get(Calendar.DAY_OF_WEEK) && Calendar.SUNDAY != start.get(Calendar.DAY_OF_WEEK)) {
+                days++;
             }
-        }catch(Exception e){
+            start.add(Calendar.DATE, 1);
         }
-        workHours = calculateProjectDuration(tasks);
-        int dailyWorkHours =0;
-        try{
-            dailyWorkHours = workHours / days;
+        int workHours = calculateProjectDuration(tasks);
+        if(days<=0||workHours<=0){
+            return 0;
         }
-        catch(ArithmeticException e){
-        }
+        int dailyWorkHours = workHours / days;
 
         return dailyWorkHours;
     }
 
-    public int calculateProjectDuration(ArrayList<Task> listOFTasks) {
-
+    public int calculateProjectDuration(ArrayList<Task> taskID) {
         int totalDuration = 0;
-        try{
-            for (int i = 0; i < listOFTasks.size(); i++) {
-                for (int j = 0; j < listOFTasks.get(i).getSubtasks().size(); j++) {
-                    totalDuration += listOFTasks.get(i).getSubtasks().get(j).getDuration();
-                }
+        for (int i = 0; i < taskID.size(); i++) {
+            for (int j = 0; j < taskID.get(i).getSubtasks().size(); j++) {
+                totalDuration += taskID.get(i).getSubtasks().get(j).getDuration();
             }
-        }catch(NullPointerException e){
         }
         return totalDuration;
     }
