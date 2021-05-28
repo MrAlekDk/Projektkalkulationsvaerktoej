46c00c15ce03ffb133622bb6a2cb6860bfb9c848
Alexander <alex030t@stud.kea.dk>
2021-05-28 11:08:55 +0200

    Added exceptions back

--- src/main/java/com/example/webapp/services/Calculator.java
+++ src/main/java/com/example/webapp/services/Calculator.java
@@ -16,36 +16,36 @@ public class Calculator {
     }
 
     public int dailyWorkHours(ArrayList<Task> tasks, Date deadline) {
-        if(tasks.isEmpty()||tasks.get(0).getSubtasks().isEmpty()){
-            return 0;
-        }
-        Date startDate = tasks.get(0).getSubtasks().get(0).getStart();
-        Calendar start = Calendar.getInstance();
-        Calendar dead = Calendar.getInstance();
+        int dailyWorkHours = 0;
+        try {
+            Date startDate = tasks.get(0).getSubtasks().get(0).getStart();
+            Calendar start = Calendar.getInstance();
+            Calendar dead = Calendar.getInstance();
 
-        start.setTime(startDate);
-        dead.setTime(deadline);
-        int days = 0;
-        while (start.before(dead)) {
-            if (Calendar.SATURDAY != start.get(Calendar.DAY_OF_WEEK) && Calendar.SUNDAY != start.get(Calendar.DAY_OF_WEEK)) {
-                days++;
+            start.setTime(startDate);
+            dead.setTime(deadline);
+            int days = 0;
+            while (start.before(dead)) {
+                if (Calendar.SATURDAY != start.get(Calendar.DAY_OF_WEEK) && Calendar.SUNDAY != start.get(Calendar.DAY_OF_WEEK)) {
+                    days++;
+                }
+                start.add(Calendar.DATE, 1);
             }
-            start.add(Calendar.DATE, 1);
+            int workHours = calculateProjectDuration(tasks);
+            if (days <= 0 || workHours <= 0) {
+                return 0;
+            }
+            dailyWorkHours = workHours / days;
+        }catch(NullPointerException e){
         }
-        int workHours = calculateProjectDuration(tasks);
-        if(days<=0||workHours<=0){
-            return 0;
-        }
-        int dailyWorkHours = workHours / days;
-
         return dailyWorkHours;
     }
 
-    public int calculateProjectDuration(ArrayList<Task> taskID) {
+    public int calculateProjectDuration(ArrayList<Task> listOfTasks) {
         int totalDuration = 0;
-        for (int i = 0; i < taskID.size(); i++) {
-            for (int j = 0; j < taskID.get(i).getSubtasks().size(); j++) {
-                totalDuration += taskID.get(i).getSubtasks().get(j).getDuration();
+        for (int i = 0; i < listOfTasks.size(); i++) {
+            for (int j = 0; j < listOfTasks.get(i).getSubtasks().size(); j++) {
+                totalDuration += listOfTasks.get(i).getSubtasks().get(j).getDuration();
             }
         }
         return totalDuration;
